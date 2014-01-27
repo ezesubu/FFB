d9a0ffd6f3063afb853e7838f24c699843829076
Ezequiel Suárez <ezesubu@gmail.com>
2014-01-27 16:46:00 -0500

    install ruby

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FFB::Application.config.secret_key_base = '801471aafc4ae6d63aeae4e381af4ec97b40a363722604c5e252bc0f9e25fcb469c75603d45f7ff85fedff486008ffcf674b3117860c416e04042980c81b3c16'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FFB::Application.config.session_store :cookie_store, key: '_FFB_session'
